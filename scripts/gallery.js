e04f5daa915eba67f157572788133e8cfa69ca55
Harabec92 <marcel.berendt@web.de>
2018-04-12 21:37:39 +0200

    dd

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,3 +2,8 @@ function functionOne() {
   var x = document.getElementById("image1");
   x.style = "background-image: url('images/camera.jpg')"
 }
+
+function fillGalery() {
+  var x = document.getElementById("image1");
+  x.style = "background-image: url('images/camera.jpg')"
+}
